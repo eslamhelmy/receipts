8e94d61ecdab52597ce04b6df450bbc377479c21
eslamhelmy524 <eslamhelmy524@gmail.com>
2026-01-05 08:17:15 +0400

    feat(infra): Add OutboxMessages table migration

--- Receipts.Infrastructure/Migrations/20260105041623_AddOutboxTable.Designer.cs
+++ Receipts.Infrastructure/Migrations/20260105041623_AddOutboxTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Receipts.Infrastructure;
 
@@ -11,9 +12,11 @@ using Receipts.Infrastructure;
 namespace Receipts.Infrastructure.Migrations
 {
     [DbContext(typeof(ReceiptsDbContext))]
-    partial class ReceiptsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260105041623_AddOutboxTable")]
+    partial class AddOutboxTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
